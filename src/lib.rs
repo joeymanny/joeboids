04512f7613d8df9a7c9ba3db4b4258f1306fbbd0
joeymanny <116774114+joeymanny@users.noreply.github.com>
2023-09-11 07:51:34 -0800

    sensical data structuring
    also i couldn't figure out how to use cli for this im so dumb with git omfg

--- src/lib.rs
+++ src/lib.rs
@@ -1,33 +1,56 @@
-trait BoidCanvas {
+use std::ops::Add;
+pub trait BoidCanvas {
     fn draw_triangle(&mut self, p1: (i32, i32), p2: (i32, i32), p3: (i32, i32)) -> Result<(), String>;
-    fn new_boid() -> Boid {
+}
+struct Boid<'a,T: BoidCanvas> {
+    canvas: &'a mut T,
+    b0: Vec<i32>,
+    b1: Vec<i32>,
+    switch: bool,
+}
+impl<U: BoidCanvas> Boid<'_, U> {
+    fn new<T: BoidCanvas> (canvas: &mut T) -> Boid<T> {
         Boid {
-            canvas: &mut self,
-            b0: Vec<Boidee>::new(),
-            b1: Vec<Boidee>::new(),
+            canvas: canvas,
+            b0: Vec::<i32>::new(),
+            b1: Vec::<i32>::new(),
             switch: false,
         }
     }
 }
-struct Boid<T: BoidCanvas> {
-    canvas: &mut T,
-    b0: Vec<Boidee>,
-    b1: Vec<Boidee>,
-    switch: Bool,
+struct Boidee {
+    pos: Vector2,
+    dir: Angle,
+    speed: f32,
 }
-impl Boid {
-    step(&mut self) {
-        if self.switch {
-            self.b1.update_boids(&mut self.b0);
-            switch = !switch;
-        } else {
-            self.b0.update_boids(&mut self.b1);
-            switch = !switch;
+struct Angle {
+    rad: f32
+}
+impl Angle {
+    fn new (x: f32) -> Angle {
+        Angle {
+            rad: x
         }
     }
 }
-impl Vec<Boidee> {
-    fn update_boids(&) {
-        
+struct Vector2 {
+    x: f32,
+    y: f32,
+}
+impl Vector2 {
+    fn new(x: f32, y: f32) -> Vector2 {
+        Vector2 {
+            x: x,
+            y: y,
+        }
+    }
+}
+impl Add for Vector2 {
+    type Output = Vector2;
+    fn add(self, rhs: Vector2) -> Self::Output {
+        Vector2 {
+            x: self.x + rhs.x,
+            y: self.y + rhs.y,
+        }
     }
 }
