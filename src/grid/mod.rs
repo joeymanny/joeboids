969abff3395be452e9e9f1e39b6da93f94b1679e
joeymanny <joeymanny1212@gmail.com>
2023-11-08 22:37:02 -0900

    cleanup and using features

--- src/grid/mod.rs
+++ src/grid/mod.rs
@@ -1,5 +1,7 @@
 use crate::LOCAL_SIZE;
+use crate::boid::Boid;
 use crate::boidee::Boidee;
+use crate::vector2::Vector2;
 #[derive(Clone, Debug)]
 pub struct Grid{
     max: (f32, f32),
@@ -17,7 +19,7 @@ impl Grid{
         Self { max, min, cells, fac: 1.0/fac }
     }
     pub fn from_vec(data: Vec<Boidee>, fac: f32) -> Grid{
-        let fac = 1.0 / fac; // dividing is slow
+        // fac is how large the cells should be, it can be a third of a boids fov (i think)
         // make an array of cells of the right size
         // populate the Vec's of the cells with references to the data
         // profit
@@ -29,9 +31,11 @@ impl Grid{
         //      3: 0|1|2|3|4
         //      4: 0|1|2|3|4
 
+        let fac = 1.0 / fac; // dividing is slow
         let mut min = (0.0, 0.0);
         let mut max = (0.0, 0.0);
         for boid in data.iter(){
+            // dynamically resize
             if boid.position.x > max.0{
                 max.0 = boid.position.x;
             }
@@ -47,24 +51,22 @@ impl Grid{
         }
         // empty 3D array (3rd dimension for Boidees)
         // same as in new(): maximum size for the boids given
-        let mut buf: Vec<Vec<Vec<Boidee>>> = Grid::init_grid_vec(((max.0 - min.0).floor() as usize + 1, (max.1 - min.1).floor() as usize + 1), fac);
-        // the buffer has positive indexes, but the boids may have nagative postitions
-
+        let mut cells: Vec<Vec<Vec<Boidee>>> = Grid::init_grid_vec(((max.0 - min.0).floor() as usize + 1, (max.1 - min.1).floor() as usize + 1), fac);
         // fill them with data
         // this will panic is max is too small, so make sure max isn't too small
         for boidee in data{
+            // we subtract min so we get an index > 0
             let index_x = ((boidee.position.x - min.0) * fac).floor() as usize;
             let index_y = ((boidee.position.y - min.1) * fac).floor() as usize;
-            buf[index_x][index_y].push(boidee);
+            cells[index_x][index_y].push(boidee);
         }
-        Self { max, min, cells: buf, fac }
+        Self { max, min, cells, fac }
     }
     pub fn get_cell_neighbors(&self, sub: &Boidee) -> Vec<Boidee>{
         // we need to subtract min from the postition before using it as an index
-        let mut rtrn: Vec<Boidee> = vec![];
+        let mut neighbors: Vec<Boidee> = vec![];
         let index_x: usize = ((sub.position.x - self.min.0) * self.fac).floor() as usize;
         let index_y: usize = ((sub.position.y - self.min.1) * self.fac).floor() as usize;
-        let our_cell = self.cells[index_x][index_y].clone();
         let x_0 = index_x <= 1;
         let y_0 = index_y <= 1;
         let y_max = index_y >= ((self.max.1 - self.min.1) as f32 * self.fac).floor() as usize;
@@ -72,44 +74,58 @@ impl Grid{
         
         if !x_0{
             // left
-            rtrn.append(&mut self.cells[index_x - 1][index_y].clone());
+            neighbors.append(&mut self.cells[index_x - 1][index_y].clone());
             if !y_0{
                 //upper left
-                rtrn.append(&mut self.cells[index_x - 1][index_y - 1].clone());
+                neighbors.append(&mut self.cells[index_x - 1][index_y - 1].clone());
             }
             if !y_max{
                 // lower left
-                rtrn.append(&mut self.cells[index_x - 1][index_y + 1].clone());
+                neighbors.append(&mut self.cells[index_x - 1][index_y + 1].clone());
             }
-        }else{
-            // rtrn.append(&mut self.cells[self.cells.len() - 1][y_adj].clone());
         }
         if !x_max{
             //right
-            rtrn.append(&mut self.cells[index_x + 1][index_y].clone());
+            neighbors.append(&mut self.cells[index_x + 1][index_y].clone());
             if !y_0{
                 // upper right
-                rtrn.append(&mut self.cells[index_x + 1][index_y - 1].clone());
+                neighbors.append(&mut self.cells[index_x + 1][index_y - 1].clone());
             }
             if !y_max{
                 // lower right
-                rtrn.append(&mut self.cells[index_x + 1][index_y + 1].clone());
+                neighbors.append(&mut self.cells[index_x + 1][index_y + 1].clone());
             }
         }
         if !y_max{
             // down
-            rtrn.append(&mut self.cells[index_x][index_y + 1].clone());
+            neighbors.append(&mut self.cells[index_x][index_y + 1].clone());
         }
         if !y_0{
             // up
-            rtrn.append(&mut self.cells[index_x][index_y - 1].clone());
+            neighbors.append(&mut self.cells[index_x][index_y - 1].clone());
         }
         // we also need our own cell of course
-        rtrn.append(&mut our_cell.clone());
-        rtrn.append(&mut self.cells[self.cells.len() - 1][self.cells[self.cells.len() - 1].len() - 1].clone());
+        neighbors.append(&mut self.cells[index_x][index_y].clone());
+        // neighbors.append(&mut self.cells[self.cells.len() - 1][self.cells[self.cells.len() - 1].len() - 1].clone());
 
-        rtrn
+        neighbors
     }
+cfg_if::cfg_if!{
+if #[cfg(feature = "visualize_neighbors")]{
+    pub fn random(num: usize, min: (f32, f32), max: (f32, f32)) -> Grid{
+        let mut v:Vec<Boidee> = vec![];
+        let mut chosen = Boidee{
+            chosen: true,
+            ..Boidee::random(min, max)
+        };
+        v.push(chosen);
+        for _ in 1..num {
+            v.push(Boidee::random(min, max));
+        }
+        Grid::from_vec(v, LOCAL_SIZE)
+
+    }
+}else{
     pub fn random(num: usize, min: (f32, f32), max: (f32, f32)) -> Grid{
         let mut v:Vec<Boidee> = vec![];
         for _ in 0..num {
@@ -118,6 +134,8 @@ impl Grid{
         Grid::from_vec(v, LOCAL_SIZE)
 
     }
+}
+}
     pub fn init_num(num: u32) -> Grid{
         let mut v:Vec<Boidee> = vec![];
         for _ in 0..num{
