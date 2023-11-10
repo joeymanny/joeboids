fb5c8f5576e4ea0c173456038b9d59077326d507
joeymanny <joeymanny1212@gmail.com>
2023-11-09 20:26:54 -0900

    reexported TargetType and documented

--- src/lib.rs
+++ src/lib.rs
@@ -2,14 +2,15 @@
 mod tests;
 
 mod grid;
-mod angle;
 pub mod boid;
 mod vector2;
-mod boidee;
-pub const TOO_CLOSE: f32 = 15.0;
+pub mod boidee;
 pub const LOCAL_SIZE: f32 = 50.0;
-pub const MAX_RAND_SCOPE: usize = 100;
-pub const  SCHEDULE_NANOS: u64 = 16_666_666; // 120 fps
+pub use boid::TargetType;
+
+/// Trait required for the [`Boid`](crate::boid::Boid) object to know how to draw [`Boidee`](crate::boidee::Boidee)s.
+/// An object implementing this trait must be passed to your [`crate::boid::Boid`] when calling `step_draw` and
+/// related functions
 pub trait BoidCanvas {
     fn draw_triangle(
         &mut self,
