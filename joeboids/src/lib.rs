3b957514a4e350a0486a78f28e68ff1d0b308ea4
joeymanny <joeymanny1212@gmail.com>
2023-11-13 20:50:20 -0900

    added reexports and updated trait to use world units

--- joeboids/src/lib.rs
+++ joeboids/src/lib.rs
@@ -4,6 +4,8 @@ mod vector2;
 pub mod boidee;
 pub const LOCAL_SIZE: f32 = 50.0;
 pub use boid::TargetType;
+pub use boid::Boid;
+pub use boidee::Boidee;
 
 /// Trait required for the [`Boid`](crate::boid::Boid) object to know how to draw [`Boidee`](crate::boidee::Boidee)s.
 /// An object implementing this trait must be passed to your [`crate::boid::Boid`] when calling `step_draw` and
@@ -11,8 +13,8 @@ pub use boid::TargetType;
 pub trait BoidCanvas {
     fn draw_triangle(
         &mut self,
-        p1: (i32, i32),
-        p2: (i32, i32),
-        p3: (i32, i32),
+        p1: (f32, f32),
+        p2: (f32, f32),
+        p3: (f32, f32),
     ) -> Result<(), String>;
 }
