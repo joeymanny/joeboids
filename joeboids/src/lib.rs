1429437293d6d30a79f33091708c2f5110e29a5e
joeymanny <joeymanny1212@gmail.com>
2023-11-13 21:33:28 -0900

    abolished LOCAL_SIZE const

--- joeboids/src/lib.rs
+++ joeboids/src/lib.rs
@@ -2,13 +2,12 @@ mod grid;
 pub mod boid;
 mod vector2;
 pub mod boidee;
-pub const LOCAL_SIZE: f32 = 50.0;
 pub use boid::TargetType;
 pub use boid::Boid;
 pub use boidee::Boidee;
 
 /// Trait required for the [`Boid`](crate::boid::Boid) object to know how to draw [`Boidee`](crate::boidee::Boidee)s.
-/// An object implementing this trait must be passed to your [`crate::boid::Boid`] when calling `step_draw` and
+/// An object implementing this trait must be passed to your [Boid](`crate::boid::Boid`) when calling `step_draw` and
 /// related functions
 pub trait BoidCanvas {
     fn draw_triangle(
